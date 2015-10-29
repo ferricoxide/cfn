786b48f8ebbb763294d1a7bd1001e7ccb817141f
Thomas H Jones II <github@xanthia.com>
2015-10-29 17:15:08 -0400

    Install missing dependencies

--- scripts/RedMine/MiscDeps.sh
+++ scripts/RedMine/MiscDeps.sh
@@ -4,3 +4,34 @@
 # installation of RedMine
 #
 #################################################################
+
+KNOWNDEPS=(
+      mariadb
+      ImageMagick
+      ImageMagick-devel
+      libcurl-devel
+      httpd-devel
+      mariadb-devel
+      ipa-pgothic-fonts
+      cyrus-sasl
+   )
+
+NEEDDEPS=""
+
+# Check to see which dependencies remain to be satisfied
+for DEPCHK in ${KNOWNDEPS[@]}
+do
+   if [[ $(rpm --quiet -q "${DEPCHK}") -ne 0 ]]
+   then
+      NEEDDEPS="${NEEDDEPS} + ${DEPCHK}"
+   fi
+done
+
+# Install remaining dependencies
+printf "Installing ${NEEDDEPS}... "
+if [[ $(yum --quiet install -y "${NEEDDEPS}" )$? -eq ]]
+then
+   echo "Success!"
+else
+   echo "Failed!" > /dev/stderr
+fi
